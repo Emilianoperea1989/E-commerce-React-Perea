6a5b4149cc04cbde2fa70a83ee926d90aef024b6
Emiliano Perea <emilianoperea10@gmail.com>
2022-12-08 12:16:02 -0300

    Agregando funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import NavBar from './components/navBar/NavBar';
 import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer';
+import Error404 from './components/error404/Error404';
+
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
-     <ItemListContainer greeting={"Bienvenidos a Free-Market"}/>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path={'/'} element={<ItemListContainer/>}/>
+        <Route path={'/category/:category'} element={<ItemListContainer/>}/>
+        <Route path={'/item/:id'} element={<ItemDetailContainer/>}/>
+        <Route path={'*'} element={<Error404/>}/>
+      </Routes>  
+      </BrowserRouter>
+   
     </div>
   );
 }
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { products } from '../fetchProducts/data'
+import { fetchProducts } from '../fetchProducts/fetchProducts'
+import ItemList from '../itemList/ItemList'
+import ItemCount from '../itemCount/ItemCount'
+import { useParams } from 'react-router-dom'
+import Loader from '../loader/Loading'
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = () => {
+
+    const [listProducts, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const { category } = useParams()
+
+
+
+    useEffect(() => {
+        setLoading(true)
+        fetchProducts(category ? products.filter(listProducts => listProducts.category === category) : products)
+            .then(res => {
+                setProducts(res)
+                setLoading(false)
+            })
+    }, [category])
+
+
     return (
         <div className='container'>
             <div className='row '>
-                <div className='col col-lg-12  text-center '>
-                    <h2>{greeting}</h2>
-                </div>
+               { loading ?
+                    <Loader/>:                         
+                    <ItemList listProducts={listProducts} /> }
+                    <div className='col col-lg-12  text-center '>
+                     <ItemCount initial={1} stock={4} onAdd={() => {}}/> 
+                    </div>                  
             </div>
         </div>
     )
